91c4cb13e7c4f56cd81f6fd872106967e8c6219b
vincentbwalker <vincethewalker@gmail.com>
2018-05-24 15:49:16 +0000

    Added Gravatar for chef

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def gravatar_for(user, options = {size: 80})
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase) # Get this from: google - "gravatar digest" / Ruby Image requests / # create the md5 hash
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.chefname, class: "img-rounded" )
+  end
+  
 end
