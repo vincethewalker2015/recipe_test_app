ab5f6643480dd5dc49321e4729068a98670aef53
vincentbwalker <vincethewalker@gmail.com>
2018-05-28 19:02:47 +0000

    Added new recipe creation with tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -18,9 +18,9 @@ class RecipesTest < ActionDispatch::IntegrationTest
     get recipes_path
     assert_template 'recipes/index'
     #assert_match @recipe.name, response.body - No longer valid as we are testing for links as well!!
-    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipes_path(@recipe), text: @recipe.name
     #assert_match @recipe2.name, response.body
-    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+    assert_select "a[href=?]", recipes_path(@recipe2), text: @recipe2.name
   end
   
   test "Should get to recipes show" do
@@ -29,6 +29,30 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+  end
+  
+  test "Create a new valid recipe" do
+    get new_recipe_path
+    assert_template "recipes/new"
+    name_of_recipe = "Chicken saute"
+    description_of_recipe = "great chicken dish"
+    assert_difference "Recipe.count", 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe}}
+    end
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
+  end
+  
+  test "Reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template "recipes/new"
+    assert_no_difference "Recipe.count" do
+      post recipes_path, params: { recipe: { name: " ", description: " "}}
+    end
+    assert_template "recipes/new"
+    assert_select "h2.panel-title"
+    assert_select "div.panel-body"
   end
   
   
