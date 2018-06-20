d453fe1a7be256c70cf1074be5d2ec17087a40a9
vincentbwalker <vincethewalker@gmail.com>
2018-06-20 13:08:31 +0000

    Resolved teat failure in chef's listing test

--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -22,6 +22,7 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
   end
   
   test "Should delete chef" do
+    sign_in_as(@chef2, "password")
     get chefs_path
     assert_template "chefs/index"
     assert_difference "Chef.count", -1 do
