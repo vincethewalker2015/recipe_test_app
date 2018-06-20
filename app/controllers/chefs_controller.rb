2e26b3243a0ca1c7473c9fdbb56a8014d3e38536
vincentbwalker <vincethewalker@gmail.com>
2018-06-20 12:55:53 +0000

    Added new sign in link to sessions/new

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,7 @@
 class ChefsController < ApplicationController
-  before_action :set_chef, only: [:show, :edit, :update, :destroy]
+  before_action :set_chef, only: [:show, :edit, :update, :destroy] #refer to line 58
   #before_action :require_user, except: [:index, :show]
-  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy] #refer to line 62
   
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 4) #This will paginate the page
