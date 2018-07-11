3e8ff34f62e3d3a213df446b9f430cbee2b9d2a6
vincentbwalker <vincethewalker@gmail.com>
2018-07-11 13:22:03 +0000

    Completed Comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   #get 'recipes/new', to: 'recipes#new', as: 'new_recipe'
   #get 'recipes/:id', to: 'recipes#show'
   
-  resources :recipes
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
   
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
