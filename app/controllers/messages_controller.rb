a3fda6e80b236afa7b51f4b28ce4a90dde28d7e0
vincentbwalker <vincethewalker@gmail.com>
2018-07-20 14:20:33 +0000

    Completed ChefChat function to include realtime

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,9 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chef = current_chef
     if @message.save
-      redirect_to chat_path
+      #redirect_to chat_path (This was changed to Incorporate ActionCable below. The mathod differs to comments but both ways will be ok)
+      ActionCable.server.broadcast 'chatroom_channel', message: render_message(@message), 
+                                                       chef: @message.chef.chefname
     else
       render 'chatrooms/show'
     end
@@ -16,6 +18,10 @@ class MessagesController < ApplicationController
   
   def message_params
     params.require(:message).permit (:content)
+  end
+  
+  def render_message(message)
+    render(partial: 'message', locals: { message: message} )
   end
   
 end
