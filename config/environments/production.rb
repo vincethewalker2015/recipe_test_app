de1104bcca7af45637b8b36184ae789306918b8f
vincentbwalker <vincethewalker@gmail.com>
2018-08-03 12:55:40 +0000

    removed the redis function

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.web_socket_server_url = "wss://vincethewalker-recipes.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://vincethewalker-recipes.herokuapp.com', 'http://vincethewalker-recipes.herokuapp.com']
+  #config.web_socket_server_url = "wss://vincethewalker-recipes.herokuapp.com/cable"
+  #config.action_cable.allowed_request_origins = ['https://vincethewalker-recipes.herokuapp.com', 'http://vincethewalker-recipes.herokuapp.com']
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis.new(url: ENV["REDIS_URL"]) if Rails.env.production?
+#$redis = Redis.new(url: ENV["REDIS_URL"]) if Rails.env.production?
